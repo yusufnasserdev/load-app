fb0ca04022d74933590b3c3821c23b2586e196f1
yusufnasserdev <yusufnassereng@gmail.com>
2022-09-07 08:49:50 +0200

    Implemented notifications and finished main activity

--- load-app/app/src/main/java/com/udacity/ui/MainActivity.kt
+++ load-app/app/src/main/java/com/udacity/ui/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.udacity.ui
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.udacity.R
+import com.udacity.util.sendNotification
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -22,16 +27,24 @@ class MainActivity : AppCompatActivity() {
     private var downloadID: Long = 0
 
     private lateinit var notificationManager: NotificationManager
-    private lateinit var pendingIntent: PendingIntent
-    private lateinit var action: NotificationCompat.Action
+    private lateinit var downloadedFile: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        notificationManager = ContextCompat.getSystemService(
+            this,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        createChannel(
+            getString(R.string.download_notification_channel_id),
+            getString(R.string.download_notification_channel_name)
+        )
 
         custom_button.setOnClickListener {
 
@@ -39,12 +52,15 @@ class MainActivity : AppCompatActivity() {
 
             when (radio_group.checkedRadioButtonId) {
                 R.id.glide_button -> {
+                    downloadedFile = getString(R.string.glide)
                     selectedURL = GLIDE_URL
                 }
                 R.id.retrofit_button -> {
+                    downloadedFile = getString(R.string.retrofit)
                     selectedURL = RETROFIT_URL
                 }
                 R.id.repo_button -> {
+                    downloadedFile = getString(R.string.repo)
                     selectedURL = REPO_URL
                 }
                 else -> {
@@ -55,12 +71,6 @@ class MainActivity : AppCompatActivity() {
             }
 
             download(selectedURL)
-        }
-    }
-
-    private val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
         }
     }
 
@@ -76,7 +86,76 @@ class MainActivity : AppCompatActivity() {
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
-        custom_button.animationIsOver()
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            custom_button.animationIsOver()
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+
+            if (id == downloadID) {
+                // Requesting an instance of Download Manager
+                val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+
+                // Querying the downloads by our downloadID and storing the results
+                val downloads = downloadManager.query(DownloadManager.Query().setFilterById(id))
+
+                // Checking if the downloadID exists
+                if (downloads.moveToFirst()) {
+
+                    when (downloads.getColumnIndex(DownloadManager.COLUMN_STATUS)) {
+                        DownloadManager.STATUS_SUCCESSFUL -> {
+                            val uriColumnIndex =
+                                downloads.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI)
+                            val uri = downloads.getString(uriColumnIndex)
+
+                            notificationManager.sendNotification(
+                                downloaded = true,
+                                uri = uri,
+                                downloadedFile = downloadedFile,
+                                context = context!!
+                            )
+                        }
+                        DownloadManager.STATUS_FAILED -> {
+                            notificationManager.sendNotification(
+                                downloaded = false,
+                                uri = "",
+                                downloadedFile = downloadedFile,
+                                context = context!!
+                            )
+                        }
+                        DownloadManager.STATUS_PENDING -> {
+                            Toast.makeText(context, "Download Pending", Toast.LENGTH_SHORT).show()
+                        }
+                        DownloadManager.STATUS_PAUSED -> {
+                            Toast.makeText(context, "Download Paused", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description =
+                getString(R.string.download_channel_description)
+
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
     }
 
     companion object {
@@ -86,7 +165,10 @@ class MainActivity : AppCompatActivity() {
             "https://github.com/bumptech/glide/archive/master.zip"
         private const val RETROFIT_URL =
             "https://github.com/square/retrofit/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
 }
---
+++ load-app/app/src/main/java/com/udacity/util/Notification.kt
@@ -0,0 +1,110 @@
+package com.udacity.util
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import com.udacity.R
+import com.udacity.ui.DetailActivity
+import com.udacity.ui.MainActivity
+
+
+// Notification ID.
+private const val NOTIFICATION_ID = 0
+private const val REQUEST_CODE = 0
+private const val FLAGS = PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+
+/**
+ * Builds and delivers the notification.
+ *
+ * @param downloaded file download status
+ * @param downloadedFile downloaded file name
+ * @param uri file uri
+ * @param context activity context.
+ */
+fun NotificationManager.sendNotification(
+    downloaded: Boolean,
+    uri: String,
+    downloadedFile: String,
+    context: Context
+) {
+
+
+    // Create the content intent for the notification, which returns to the main activity
+    val contentIntent = Intent(context, MainActivity::class.java)
+
+    val contentPendingIntent = PendingIntent.getActivity(
+        context,
+        NOTIFICATION_ID,
+        contentIntent,
+        FLAGS
+    )
+
+    val detailIntent = Intent(context, DetailActivity::class.java).apply {
+        putExtra("status", downloaded)
+        putExtra("file", downloadedFile)
+        putExtra("uri", uri)
+    }
+
+    val detailPendingIntent = PendingIntent.getActivity(
+        context,
+        REQUEST_CODE,
+        detailIntent,
+        FLAGS
+    )
+
+    val statusImage: Bitmap
+    val messageBody: String
+
+    //determines if download is success or fail and sets variable attributes for the notification
+    if (downloaded) {
+        messageBody = context.getString(R.string.download_success)
+        statusImage = BitmapFactory.decodeResource(
+            context.resources,
+            R.drawable.ic_baseline_done_24
+        )
+    } else {
+        messageBody = context.getString(R.string.download_failure)
+        statusImage = BitmapFactory.decodeResource(
+            context.resources,
+            R.drawable.ic_baseline_error_24
+        )
+    }
+
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(statusImage)
+        .bigLargeIcon(null)
+
+    // Build the notification
+    val builder = NotificationCompat.Builder(
+        context,
+        context.getString(R.string.download_notification_channel_id)
+    )
+
+        .setContentTitle(context.getString(R.string.notification_title))
+        .setSmallIcon(R.drawable.ic_baseline_cloud_download_24)
+        .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setStyle(bigPicStyle)
+        .setLargeIcon(statusImage)
+        .addAction(
+            R.drawable.ic_baseline_insert_drive_file_24,
+            context.getString(R.string.notification_button),
+            detailPendingIntent
+        )
+
+    notify(NOTIFICATION_ID, builder.build())
+}
+
+/**
+ * Cancels all notifications.
+ *
+ */
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
